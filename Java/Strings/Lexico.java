43117078db778aff20573c15fc0be72f0d51ee49
Jon-Erik <jonerik@pdx.edu>
2015-08-28 17:35:23 -0700

    Fixed String Error

--- Java/Strings/Lexico.java
+++ Java/Strings/Lexico.java
@@ -43,49 +43,4 @@ public class Solution {
         
         return str;
     }
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
-public class Solution {
-
-    public static void main(String[] args) {
-        
-        Scanner sc      = new Scanner(System.in);
-        String input    = sc.nextLine();
-        int len         = sc.nextInt();
-        
-        char[] chars = input.toCharArray();
-        String high     = "";
-        String low      = "";
-        String current  = "";
-        
-        high    = setLen(len, chars, 0);
-        low     = setLen(len, chars, 0);
-        current = setLen(len, chars, 0);
-           
-        for (int i = 0; i < chars.length - (len-1); ++i){
-            if (current.compareTo(low) < 0)  { low  = current; }
-            if (current.compareTo(high) > 0) { high = current; }
-            current = setLen(len, chars, i);
-        }
-        
-        if (current.compareTo(low) < 0)  { low  = current; }
-        if (current.compareTo(high) > 0) { high = current; }
-        
-        System.out.println(low);
-        System.out.println(high);
-        
-    }
-    
-    public static String setLen(int length, char[] chars, int current) {
-        String str = "";
-        for (int i = 0; i < length; i++) {
-            str = str + chars[current++];
-        }
-        
-        return str;
-    }
 }
