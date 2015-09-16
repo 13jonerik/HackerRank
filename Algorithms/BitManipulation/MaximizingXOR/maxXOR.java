f62e24eebc6eca7636d17f86feb4ce87d76f0fc8
Jon-Erik <jonerik@pdx.edu>
2015-09-15 19:00:30 -0700

    Maximizing XOR

---
+++ Algorithms/BitManipulation/MaximizingXOR/maxXOR.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+/*
+ * Complete the function below.
+ */
+
+    static int maxXor(int l, int r) {
+        int max = 0;
+        int len = r - l;
+        for (int i = 0; i <= len; i++){
+            for (int j = 0; j < len; j++) {
+                if (((l+i) ^ (l+j)) > max){
+                    max = (l+i) ^ (l+j);
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int res;
+        int _l;
+        _l = Integer.parseInt(in.nextLine());
+        
+        int _r;
+        _r = Integer.parseInt(in.nextLine());
+        
+        res = maxXor(_l, _r);
+        System.out.println(res);
+        
+    }
+}
