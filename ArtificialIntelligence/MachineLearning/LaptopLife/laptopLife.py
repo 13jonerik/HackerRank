a664d80f5fa8e2935f0ea60ca52c6cf94dbdadf6
Jon-Erik <jonerik@pdx.edu>
2015-10-07 23:55:30 -0700

    Laptop Battery Life Linear Regression

---
+++ ArtificialIntelligence/MachineLearning/LaptopLife/laptopLife.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+data = float(sys.stdin.readline())
+
+# Enter your code here
+
+if data < 4: 
+    print 2 * data 
+else: 
+    print 8.00
+
+
